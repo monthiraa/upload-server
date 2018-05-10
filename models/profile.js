ddf0c28a7efc381a15c95297cb16ca8b126e2c0b
v9nineIX <v9nineix@gmail.com>
2018-05-10 15:12:28 +0700

    Add Profile model.

---
+++ models/profile.js
@@ -0,0 +1,43 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var profileSchema = new Schema({
+  mediaType: {
+    type: Schema.Types.String,
+  },
+  saveDist: {
+    type: Schema.Types.Boolean
+  },
+  config: {
+    type: {
+       width: {type: Schema.Types.Number},
+       height: {type: Schema.Types.Number},
+       quality: {type: Schema.Types.Number},
+       outputType: {type: Schema.Types.String}
+       }
+    },
+  deletetd: {
+    type :Schema.Types.Boolean
+  },
+  path: {
+    type: Schema.Types.String,
+  },
+  createAt: {
+    type: Schema.Types.Date,
+    default: Date.now
+  },
+  updateAt: {
+    type: Schema.Types.Date,
+    default: Date.now
+  },  
+  serviceId: {
+    type: Schema.Types.ObjectId,
+    ref: 'Service'
+  },    
+  
+
+});
+
+// Compile model from schema
+var Profile = mongoose.model('Profile', profileSchema);
+module.exports = Profile;
